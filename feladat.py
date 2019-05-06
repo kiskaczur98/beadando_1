b194fb0d091cca417ce879835831761d0d3c03e7
PETRA <kiskaczur98@gmail.com>
2019-05-06 23:38:49 +0200

    oszto

--- feladat.py
+++ feladat.py
@@ -1,2 +1,19 @@
-for i in range(1,101):
-    print(i)
+
+def isprim(val):
+    if val<2:
+        return False
+    for i in range(2,val):
+        if val%i==0:
+            return False
+    return True
+
+def oszto(x,y):
+    if x > y:
+        temp = x
+        x = y
+        y = temp
+    for i in range(x+1,1,-1):
+            if x%i==0 and isprim(i)==True:
+                return i
+
+print(oszto(6,8))

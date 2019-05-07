3f6a47f107f1649e66d325ab0fe9f1cf20191d8a
PETRA <kiskaczur98@gmail.com>
2019-05-07 12:16:52 +0200

    osztomaskepp

--- feladat.py
+++ feladat.py
@@ -1,19 +1,19 @@
 
-def isprim(val):
-    if val<2:
+def isprim(a):
+    if a < 1:
         return False
-    for i in range(2,val):
-        if val%i==0:
+    if a == 2:
+        return True
+    if a % 2 == 0:
+        return False
+    for i in range(2, a // 2):
+        if a % i == 0:
             return False
     return True
 
 def oszto(x,y):
-    if x > y:
-        temp = x
-        x = y
-        y = temp
     for i in range(x+1,1,-1):
             if x%i==0 and isprim(i)==True:
                 return i
 
-print(oszto(6,8))
+print(oszto(8,6))

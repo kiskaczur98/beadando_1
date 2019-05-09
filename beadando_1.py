5569e5b3746c0e3e1e93fe82a10dd98d8d145f67
PETRA <kiskaczur98@gmail.com>
2019-05-09 19:44:47 +0200

    beadando_1

--- beadando_1.py
+++ beadando_1.py
@@ -3,17 +3,17 @@
 # számokat, amelyekre kell kettőt emelni és a hatványok összege n.
 
 while True:
-    try:
-        def elso_feladat(szam):
-            hatvany=0
-            binaris=list(str(bin(szam)[2:]))
-            kitevok=[]
-            for i in range(len(binaris)-1,-1,-1):
-                if binaris[i]=='1':
-                    kitevok.append(hatvany)
-                hatvany=hatvany+1
-            return kitevok
 
+    def elso_feladat(szam):
+        hatvany=0
+        binaris=list(str(bin(szam)[2:]))
+        kitevok=[]
+        for i in range(len(binaris)-1,-1,-1):
+            if binaris[i]=='1':
+                kitevok.append(hatvany)
+            hatvany=hatvany+1
+        return kitevok
+    try:
         n=int(input('Adj meg egy számot!'))
         print(elso_feladat(n))
     except ValueError:

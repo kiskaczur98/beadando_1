a72817576a413dceeee4e8d856ec72c198176d76
PETRA <kiskaczur98@gmail.com>
2019-05-15 14:42:13 +0200

    beadando_7

--- beadando_1.py
+++ beadando_1.py
@@ -1,17 +1,14 @@
-# Minden pozitív szám felírható kettő pozitív (plusz nulla) hatványainak összegeként.
-# Írjon függvényt, amely egy adott n szám esetén egy rendezett listában visszaad a
-# számokat, amelyekre kell kettőt emelni és a hatványok összege n.
 
+def elso_feladat(szam):
+    hatvany=0
+    binaris=list(str(bin(szam)[2:]))
+    kitevok=[]
+    for i in range(len(binaris)-1,-1,-1):
+        if binaris[i]=='1':
+            kitevok.append(hatvany)
+        hatvany=hatvany+1
+    return kitevok
 while True:
-    def elso_feladat(szam):
-        hatvany=0
-        binaris=list(str(bin(szam)[2:]))
-        kitevok=[]
-        for i in range(len(binaris)-1,-1,-1):
-            if binaris[i]=='1':
-                kitevok.append(hatvany)
-            hatvany=hatvany+1
-        return kitevok
     try:
         n=int(input('Adj meg egy számot!'))
         print(elso_feladat(n))
